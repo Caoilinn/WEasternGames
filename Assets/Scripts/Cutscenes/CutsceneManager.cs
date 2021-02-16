e00449935f61aa7546a799931db8f7e56144231b
Nicholas Daryl <Nicholas.Daryl@prometric.com>
2021-02-16 22:39:06 +0000

    /#30619 - Tests out triggering Cutscenes
    A cutscene will be triggered when all enemies have been defeated
    Will disable player controls and camera when cutscene triggers
    Plays around with playing animations from characters
    Tests playing sound files
    Tests moving the camera

---
+++ Assets/Scripts/Cutscenes/CutsceneManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject player;
+    public GameObject playerCamera;
+    public GameObject cutsceneCamera;
+    private bool isAllDead = false;
+    private PlayableDirector playableDirector;
+
+    void Start() {
+        playableDirector = GetComponent<PlayableDirector>();
+        playableDirector.played += OnCutscenePlayed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < enemies.Length; i++) {
+            if (enemies[i].activeSelf) {
+                return;
+            }
+        }
+
+        // else if there are no more enemies
+        PlayCutscene();
+    }
+
+    private void PlayCutscene() {
+        //Debug.Log("Cutscene is Played");
+        if (playableDirector.state != PlayState.Playing) {
+            playableDirector.Play();
+        }
+    }
+
+    void OnCutscenePlayed(PlayableDirector director) {
+        // deactivate the player
+        player.GetComponent<PlayerMovement>().enabled = false;
+        player.GetComponent<PlayerAnimation>().enabled = false;
+        player.GetComponent<Rigidbody>().isKinematic = true;
+
+        // deactivate the player camera
+        playerCamera.SetActive(false);
+
+        // activate the audio Listener in the cutscene camera
+        cutsceneCamera.GetComponent<AudioListener>().enabled = true;
+    }
+}
